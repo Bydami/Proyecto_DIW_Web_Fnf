11302819bc73dc6357d43ea795f64bc32e119d6b
Damián Tortosa <gcasual160@gmail.com>
2025-01-29 11:29:29 +0100

    Index responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const footer = document.querySelector("footer");
 const aside = document.querySelector("aside");
-
+const menu_btn = document.querySelector(".menu-btn")
 // Configura el Intersection Observer
 const observer = new IntersectionObserver(
   (entries) => {
@@ -8,9 +8,11 @@ const observer = new IntersectionObserver(
       if (entry.isIntersecting) {
         // El footer está visible
         aside.style.top = "8%";
+        menu_btn.style.top = "33%";
       } else {
         // El footer no está visible
         aside.style.top = "15%";
+        menu_btn.style.top = "40%";
       }
     });
   },
@@ -22,3 +24,8 @@ const observer = new IntersectionObserver(
 
 // Observa el footer
 observer.observe(footer);
+
+function toggleMenu() {
+  aside.classList.toggle("active");
+  menu_btn.classList.toggle("menu-btn-active");
+}
