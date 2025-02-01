6c10775fb8f52d63fd1408099463c8fe47ad6ae9
Damián Tortosa <gcasual160@gmail.com>
2025-02-01 20:00:30 +0100

    Novedades y mods terminadas

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 const footer = document.querySelector("footer");
 const aside = document.querySelector("aside");
 const menu_btn = document.querySelector(".menu-btn")
-// Configura el Intersection Observer
+
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        // El footer está visible
         aside.style.top = "8%";
         menu_btn.style.top = "33%";
       } else {
-        // El footer no está visible
         aside.style.top = "15%";
         menu_btn.style.top = "40%";
       }
     });
   },
   {
-    root: null, // Observa con respecto al viewport
-    threshold: 0.4 // Detecta cuando al menos el 10% del footer está visible
+    root: null,
+    threshold: 0.4
   }
 );
 
-// Observa el footer
 observer.observe(footer);
 
 function toggleMenu() {
